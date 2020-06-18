71bc7fb99e6f26b3bb0c23802c984cd7431c3902
machengim <macheng83@gmail.com>
2020-06-18 22:34:30 +1000

    modify code

--- experiments/rust-server-db/src/main.rs
+++ experiments/rust-server-db/src/main.rs
@@ -3,7 +3,7 @@ use sqlx::Row;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "postgres://postgres:GJCKu5lMFLmLIvCy@10.214.144.3:5432/test";
+    let url = "postgres://postgres:MryMrHNxr8zsw3e4@10.0.208.3:5432/test";
     let pool = sqlx::PgPool::new(&url).await?;
 
     let mut cursor = sqlx::query("select * from customer").fetch(&pool);
